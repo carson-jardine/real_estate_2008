12a839e79905857126ddbb26ebcd3ab56909f22f
Carson Jardine <carsonjardine@gmail.com>
2020-08-26 09:49:33 -0600

    Write rooms by category test

--- test/house_test.rb
+++ test/house_test.rb
@@ -110,13 +110,33 @@ class HouseTest < Minitest::Test
     room_2 = Room.new(:bedroom, 11, '15')
     room_3 = Room.new(:living_room, 25, '15')
     room_4 = Room.new(:basement, 30, '41')
+    room_5 = Room.new(:dining_room, 20, '10')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    house.add_room(room_5)
+
+    expected = [room_1, room_2, room_5, room_3, room_4]
+    assert_equal expected, house.rooms_sorted_by_area
+  end
+
+  def test_it_can_find_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
 
     house.add_room(room_1)
     house.add_room(room_2)
     house.add_room(room_3)
     house.add_room(room_4)
 
-    assert_equal [room_1, room_2, room_3, room_4], house.rooms_sorted_by_area
+    expected = ({bedroom: [room_1, room_2], living_room: [room_3], basement: [room_4]})
+
+    assert_equal expected, house.rooms_by_category
   end
 end
 
