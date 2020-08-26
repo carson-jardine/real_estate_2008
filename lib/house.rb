27a0aa12ac6c50b72603482140ea698aea34aa7b
Carson Jardine <carsonjardine@gmail.com>
2020-08-26 09:21:14 -0600

    Add address method

--- lib/house.rb
+++ lib/house.rb
@@ -1,12 +1,12 @@
 class House
-
+  attr_reader :address
+  
   def initialize(price, address)
     @price = price
-
+    @address = address
   end
 
   def price
-    @price.delete("$").to_i 
-
+    @price.delete("$").to_i
   end
 end
