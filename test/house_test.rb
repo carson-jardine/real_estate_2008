43e4c5f555499bb31ae0da7dda67956610f01ef8
Carson Jardine <carsonjardine@gmail.com>
2020-08-26 09:24:32 -0600

    Create add room method and pass tests

--- test/house_test.rb
+++ test/house_test.rb
@@ -26,23 +26,17 @@ class HouseTest < Minitest::Test
   def test_rooms_start_empty
     house = House.new("$400000", "123 sugar lane")
 
-    assert_equal [], house.rooms 
+    assert_equal [], house.rooms
+  end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
   end
 end
-
-
-# house.rooms
-#=> []
-
-# room_1 = Room.new(:bedroom, 10, '13')
-#=> #<Room:0x00007fccd29b5720...>
-
-# room_2 = Room.new(:bedroom, 11, '15')
-#=> #<Room:0x00007fccd2985f48...>
-
-# house.add_room(room_1)
-
-# house.add_room(room_2)
-
-# house.rooms
-#=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
