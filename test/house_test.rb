dfcc8805a08e9a2162e1f3abd41dfabe3bbe7622
Carson Jardine <carsonjardine@gmail.com>
2020-08-26 09:44:27 -0600

    Write rooms sorted by area test

--- test/house_test.rb
+++ test/house_test.rb
@@ -103,33 +103,24 @@ class HouseTest < Minitest::Test
 
     assert_equal 210.53, house.price_per_square_foot
   end
+
+  def test_it_can_sort_by_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2, room_3, room_4], house.rooms_sorted_by_area
+  end
 end
 
- # house = House.new("$400000", "123 sugar lane")
-#=> #<House:0x00007fccd30375f8...>
 
- # room_1 = Room.new(:bedroom, 10, '13')
-#=> #<Room:0x00007fccd29b5720...>
-
- # room_2 = Room.new(:bedroom, 11, '15')
-#=> #<Room:0x00007fccd2985f48...>
-
- # room_3 = Room.new(:living_room, 25, '15')
-#=> #<Room:0x00007fccd383c2d0 @category=:living_room, @length=25, @width=15>
-
- # room_4 = Room.new(:basement, 30, '41')
-#=> #<Room:0x00007fccd297dc30 @category=:basement, @length=30, @width=41>
-
- # house.add_room(room_4)
-
- # house.add_room(room_1)
-
- # house.add_room(room_3)
-
- # house.add_room(room_2)
-
- # house.price_per_square_foot
-#=> 210.53
 
  # house.rooms_sorted_by_area
 #=> [#<Room:0x00007fccd297dc30...>, #<Room:0x00007fccd383c2d0...>, #<Room:0x00007fccd2985f48...>, #<Room:0x00007fccd29b5720...>]
