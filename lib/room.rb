e8fb1f8a14c90fd49bb4e44b6f57d12a1f1eefc3
Carson Jardine <carsonjardine@gmail.com>
2020-08-26 09:07:53 -0600

    Adds code to make area test pass

--- lib/room.rb
+++ lib/room.rb
@@ -2,6 +2,13 @@ class Room
   attr_reader :category
   def initialize(category, length, width)
     @category = category
+    @length = length
+    @width = width
+  end
+
+  def area
+    @length * @width.to_i
+
   end
 
 end
