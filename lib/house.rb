30c73d6963cba9b923321fbab95af17b7dc02260
Carson Jardine <carsonjardine@gmail.com>
2020-08-26 09:31:50 -0600

    Add rooms from category method

--- lib/house.rb
+++ lib/house.rb
@@ -12,6 +12,16 @@ class House
   end
 
   def add_room(room)
-    rooms << room 
+    rooms << room
+  end
+
+  def above_market_average?
+    price >= 500000
+  end
+
+  def rooms_from_category(category)
+    rooms.find_all do |room|
+      room.category == category
+    end 
   end
 end
